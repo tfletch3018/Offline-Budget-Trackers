bebd43bcc2635449977888e26ba858d91f406450
Tonya Fletcher <tfletch4504@gmail.com>
2021-04-27 14:20:46 -0400

    offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,7 +23,7 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/"))
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     );
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
