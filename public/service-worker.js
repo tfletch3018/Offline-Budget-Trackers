59612524dc8d7614ca53caf057e04d1ae14cf1bf
Tonya Fletcher <tfletch4504@gmail.com>
2021-04-27 14:35:37 -0400

    service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,15 +12,6 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
 ];
 
-// self.addEventListener('install', function (evt) {
-
-//     evt.waitUntil(
-//         caches.open(CACHE_NAME).then((cache) =>
-//         cache.addAll(FILES_TO_CACHE))
-//     );
-//     self.skipWaiting();
-// });
-
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
